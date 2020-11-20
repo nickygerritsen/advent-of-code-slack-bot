0c0c288e79d640f8597e637dcc3786cecf3d578f
Nicky Gerritsen <nicky@letstalk.nl>
2020-11-20 16:07:10 +0100

    Set up project

---
+++ src/Command/AocCommand.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpClient\HttpClient;
+
+class AocCommand extends Command
+{
+    protected static $defaultName = 'aoc';
+    protected int    $aocYear;
+    protected string $aocSessionId;
+    protected int    $aocLeaderboardId;
+    protected string $dataFile;
+    protected string $slackWebhook;
+
+    public function __construct(int $aocYear, string $aocSessionId, int $aocLeaderboardId, string $dataFile, string $slackWebhook)
+    {
+        parent::__construct(null);
+        $this->aocYear          = $aocYear;
+        $this->aocSessionId     = $aocSessionId;
+        $this->aocLeaderboardId = $aocLeaderboardId;
+        $this->dataFile         = $dataFile;
+        $this->slackWebhook     = $slackWebhook;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $client = HttpClient::create([
+            'base_uri' => 'https://adventofcode.com',
+            'headers'  => [
+                'Cookie' => sprintf('session=%s', $this->aocSessionId),
+            ],
+        ]);
+
+        $uri = sprintf('/%d/leaderboard/private/view/%d.json', $this->aocYear, $this->aocLeaderboardId);
+
+        $results = $client->request('GET', $uri);
+        $data    = $results->toArray();
+
+        if (file_exists($this->dataFile)) {
+            $currentData = json_decode(file_get_contents($this->dataFile), true);
+        } else {
+            $currentData = [];
+        }
+
+        $changed = false;
+        foreach ($data['members'] as $memberId => $member) {
+            foreach ($member['completion_day_level'] as $day => $stars) {
+                foreach ($stars as $part => $stats) {
+                    if (!isset($currentData[$memberId][$day][$part])) {
+                        $dt = date('d-m-Y H:i:s', $stats['get_star_ts']);
+                        $this->postMessage(sprintf('Huray, %s solved day %d part %d at %s', $member['name'], $day, $part, $dt));
+                        $currentData[$memberId][$day][$part] = true;
+                        $changed                             = true;
+                    }
+                }
+            }
+        }
+
+        if ($changed) {
+            $message = 'Scores changed, new leaderboard:' . PHP_EOL . PHP_EOL;
+            $rank    = 1;
+            foreach ($data['members'] as $member) {
+                $message .= sprintf('#%d, *%s*: %d points, %d stars', $rank, $member['name'], $member['local_score'], $member['stars']) . PHP_EOL;
+                $rank++;
+            }
+
+            $message .= PHP_EOL;
+            $message .= sprintf('<https://adventofcode.com/%d/leaderboard/private/view/%d|View Onlne Leaderboard>', $this->aocYear, $this->aocLeaderboardId);
+
+            $this->postMessage($message);
+        }
+
+        file_put_contents($this->dataFile, json_encode($currentData, JSON_PRETTY_PRINT));
+
+        return static::SUCCESS;
+    }
+
+    protected function postMessage(string $message)
+    {
+        $client  = HttpClient::create();
+        $payload = [
+            'icon_emoji' => ':christmas_tree:',
+            'username'   => 'Let\'s Talk AoC bot',
+            'text'       => $message,
+        ];
+
+        $client->request('POST', $this->slackWebhook, [
+            'json' => $payload,
+        ]);
+    }
+}
